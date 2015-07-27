05efc35e8e1946ae8e423c69a54837dc6d588868
Patrick <pcnicklaus@gmail.com>
2015-07-27 14:12:14 -0600

    Names basically working

--- js/namesDom.js
+++ js/namesDom.js
@@ -1,11 +1,53 @@
 
-var allNames = document.getElementsByClassName("all-names")[0].value;
+// print First Names
+var printFirstNames = document.getElementById("printFirstNames");
 
-
-var allNamesButton = document.getElementById("all-names").onclick = displayFirstNames;
+var firstNamesButton = document.getElementById("first-names").onclick = displayFirstNames;
 
 function displayFirstNames() {
-  var firstNameAnswer = document.createElement("li");
+  var firstNameAnswer = document.createElement("LI");
   firstNameAnswer.innerHTML = a;
-  document.getElementById("printFirstNames").appendChild(firstNameAnswer);
+  printFirstNames.appendChild(firstNameAnswer);
+};
+
+//print all names
+var printAllNames = document.getElementById("printAllNames");
+
+var printAllNamesButton = document.getElementById("all-names").onclick = displayAllNames;
+
+function displayAllNames() {
+  var allNamesAnswer = document.createElement("LI");
+  allNamesAnswer.innerHTML = allNames;
+  printAllNames.appendChild(allNamesAnswer)
 }
+
+
+
+// Last Names
+var printLastNames = document.getElementById("printLastNames");
+
+var lastNamesButton = document.getElementById("last-names").onclick = displayLastNames;
+
+function displayLastNames() {
+  var lastNameAnswer = document.createElement("LI");
+  lastNameAnswer.innerHTML = p;
+  printLastNames.appendChild(lastNameAnswer);
+}
+// (displayLastNames);
+
+
+// Names with lengths
+var printNamesWithLengths = document.getElementById("printNamesWithLengths");
+
+var namesLengthButton = document.getElementById("names-with-lengths").onclick = displayNamesWithLengths;
+
+function displayNamesWithLengths() {
+  var namesWithLengthsAnswer = document.createElement("LI");
+  namesWithLengthsAnswer.innerHTML = nameLengthList;
+  printNamesWithLengths.appendChild(namesWithLengthsAnswer);
+}
+// displayNamesWithLengths();
+
+
+
+
--- js/namesLogic_test.js
+++ js/namesLogic_test.js
@@ -1,31 +1,38 @@
 
+// console.log(namesArray);
+
+// count characters and print with name
+//  working
+
+
 var allNames = document.getElementsByClassName("all-names")[0].value;
 
 var namesArray = [];
-namesArray = allNames.split("\n");
+var namesArray = allNames.split("\n");
 namesArray.pop();
 var x = namesArray.toString();
 
-
-// first names  WORKING
-var firstNames = [];
-
-for (var i = 0; i < namesArray.length; i++) {
-  var a = [];
-  var y = namesArray[i].split(" ", 1);
-  firstNames.push(y);
-  a = firstNames.join("\n");
-  // console.log(a);
-  // return a;
-}
-console.log(a);
-
-// Names & Length of the Names WORKING
 var nameLengthList = "";
 for (var i = 0; i < namesArray.length; i++) {
   nameLengthList += "name: " + namesArray[i] + ", length of name is " + namesArray[i].length + " characters.\n";
 };
 console.log(nameLengthList);
+
+
+
+// first names
+// working
+var firstNames = [];
+var a = [];
+for (var i = 0; i < namesArray.length; i++) {
+  var y = namesArray[i].split(" ", 1);
+  firstNames.push(y);
+  a = firstNames.join("\n");
+  return a;
+}
+
+console.log(a);
+
 
 
 // last names as a string adn first names as an array from this function.
@@ -36,27 +43,20 @@ var p = "";
 for (var i = 0; i < namesArray.length; i++) {
   var y = namesArray[i].split(" ", 1);
   p += namesArray[i].replace(y, "");
-  // firstNames.push(y);
+  firstNames.push(y);
   // console.log(namesArray[i]);
   // console.log(lastNames);
 }
-console.log(p);
-
-
 p.trim();
 var t = p.trim();
-var h = t.split(" ");
-h.reverse();
+var x = t.split(" ");
+x.reverse();
 
 var re = [];
-var io = ""
 for (var i = 0; i < x.length; i++) {
   var io = re.shift();
   console.log(io);
-  // return io;
 };
 // console.log(firstNames);
 // console.log(lastNames);
 // console.log(p);
-
-
